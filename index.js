94eb43723a649e0d508148abe09d0eb3c2ac836e
Loes_s <103633598+LoesSsssSssssssssssssssssssss@users.noreply.github.com>
2023-02-06 16:46:22 +0300

    init

---
+++ index.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const http = require("http");
+const routerCar = require("./routes/car.route");
+
+const app = express(); //созд экземпляр экспресса
+const server = http.createServer(app); //создаём сервер
+
+const port = process.env.PORT || 3003;
+
+app.use(express.json()); //Работа с форматом json
+app.use(express.urlencoded({ extended: true })); //что бы мы моги передавать параметры в строке
+app.use(express.static("public")); // подключаем статичные файлы
+app.use((err, request, response, next) => {
+  //middleware - промежуточный обработчик
+  console.log(err);
+  response.status(500).send("server crash");
+  next();
+});
+
+app.use("/api/car", routerCar);
+
+//app.use('/', (req, res) =>{
+//    res.status(200).json('Hello word');
+//});
+
+server.listen(port, () => {
+  //говорим серверу отслеживать порт и запускаем сервер
+  console.log(`Server running on port http://localhost:${port}`);
+});
+
+module.exports = app;
